8e9f7c5764df9b5f6fc9df78d4460d5395916112
samantha-1109 <samantha@skycity.gg>
2024-07-03 15:23:31 +0800

    webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import helloWorld from './hello-world.js';
-import addImages from './add-image.js';
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+import Heading from './components/heading/heading.js';
 
-helloWorld();
-addImages();
+const heading = new Heading();
+heading.render();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry : './src/index.js',
     output : {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
     },
     mode: 'none',
     module: {
@@ -21,8 +26,54 @@ module.exports = {
             {
                 test: /\.txt/,
                 type: 'asset/source',
-                
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['@babel/plugin-proposal-class-properties'],
+
+                    }
+                }
             }
         ]
-    }
+    },
+    plugins: [
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css', 
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello World',
+            filename: 'subfolder/custom_filename.html',
+            meta: {
+                description: 'Some description'
+            }
+        }),
+
+        // {
+        //     cleanOnceBeforeBuildPatterns : [
+        //         '**/*',
+        //         path.join(process.cwd(), 'build/**/*')
+        //     ]
+        // }
+        
+    ]
 }
